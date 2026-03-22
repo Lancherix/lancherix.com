4200d817eba7ae687e849d9c78eb405b1898ad1a
lancherix <lancherix.service@gmail.com>
2026-03-22 11:53:59 -0500

    Kiara

--- src/components/NavBar/NavBarMobile.jsx
+++ src/components/NavBar/NavBarMobile.jsx
@@ -4,46 +4,48 @@ import { Link } from "react-router-dom";
 import logo1 from "../ArtWork/navBar.svg";
 
 function NavBarMobile() {
-  const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  return (
-    <div className="NavBarMobile">
+    return (
+        <div className="NavBarMobile">
 
-      {/* Top bar */}
-      <div className="NavBarMobile-Top">
+            {/* Top bar */}
+            <div className="NavBarMobile-Top">
 
-        <Link to="/">
-          <img className="NavBarMobile-Logo" src={logo1} alt="Logo"/>
-        </Link>
+                <Link to="/">
+                    <img className="NavBarMobile-Logo" src={logo1} alt="Logo" />
+                </Link>
 
-        <button
-          className="NavBarMobile-Burger"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          ☰
-        </button>
+                <button
+                    className="NavBarMobile-Burger"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+                    </svg>
+                </button>
 
-      </div>
+            </div>
 
-      {/* Fullscreen menu */}
-      <div className={`NavBarMobile-FullMenu ${menuOpen ? "open" : ""}`}>
+            {/* Fullscreen menu */}
+            <div className={`NavBarMobile-FullMenu ${menuOpen ? "open" : ""}`}>
 
-        <Link to="https://studio.lancherix.com" onClick={()=>setMenuOpen(false)}>
-          Studio
-        </Link>
+                <Link to="https://studio.lancherix.com" onClick={() => setMenuOpen(false)}>
+                    Studio
+                </Link>
 
-        <Link to="https://studio.lancherix.com/register" onClick={()=>setMenuOpen(false)}>
-          Register
-        </Link>
+                <Link to="https://studio.lancherix.com/register" onClick={() => setMenuOpen(false)}>
+                    Register
+                </Link>
 
-        <Link to="https://studio.lancherix.com/login" onClick={()=>setMenuOpen(false)}>
-          Login
-        </Link>
+                <Link to="https://studio.lancherix.com/login" onClick={() => setMenuOpen(false)}>
+                    Login
+                </Link>
 
-      </div>
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default NavBarMobile;
