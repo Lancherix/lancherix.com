475b9d5981a7e052afad546e7d8951b155b82594
lancherix <lancherix.service@gmail.com>
2026-03-22 10:50:40 -0500

    Kiara

--- src/App.js
+++ src/App.js
@@ -8,6 +8,27 @@ import Home from './components/Home/Home';
 import Reminders from './components/Reminders/Reminders';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 900);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 900);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="mobile-block">
+        <div><img src={logo} alt='Lancherix' />
+          This service is currently not available on mobile devices.
+Access it from a desktop or laptop computer.</div>
+      </div>
+    );
+  }
+
   return (
     <Router>
       <div className="App">
