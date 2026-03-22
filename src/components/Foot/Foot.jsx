39c35ae2d39883cb65a0770fc45dd7b32f537810
lancherix <lancherix.service@gmail.com>
2026-03-22 11:01:31 -0500

    Kiara

--- src/components/Foot/Foot.jsx
+++ src/components/Foot/Foot.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Foot.css';
-import { Link } from 'react-router-dom';
+{/*import { Link } from 'react-router-dom';*/}
 
 import lancherixArt from '../ArtWork/navBar.svg';
 
