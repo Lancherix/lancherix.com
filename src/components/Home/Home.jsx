ddbc683068efe9b48878583b82b59da316482693
lancherix <lancherix.service@gmail.com>
2026-08-20 17:58:48 -0500

    Kiara

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./Home.css";
 
 /* ------------------------------------------------------------------ */
 /*  CONTENT BLOCKS                                                    */
