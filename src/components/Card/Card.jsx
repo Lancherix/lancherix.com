c591969641608cd5eb9ae9449b39d3c182270306
lancherix <lancherix.service@gmail.com>
2026-08-20 17:59:40 -0500

    Kiara C

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-import '../Home/Home.css';
 
 /* ------------------------------------------------------------------ */
 /*  HOOKS                                                              */
