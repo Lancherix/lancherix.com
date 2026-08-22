061555f805f0e4d9c517125ecbf562c62d5699ec
lancherix <lancherix.service@gmail.com>
2026-08-21 20:53:24 -0500

    Imperio Japones

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -127,7 +127,7 @@ const Home = () => {
                         )
                       }
                     >
-                      {t("nav.register")}
+                      {t('card.studio.cta')}
                     </button>
                     <button
                       onClick={() =>
