a28c2a6602366577838bc94395d9d24f51b11b0e
lancherix <lancherix.service@gmail.com>
2026-08-21 20:18:51 -0500

    El apego

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,8 +5,8 @@ import "./Home.css";
 import left from "../ArtWork/icons/arrowLeft.svg";
 import right from "../ArtWork/icons/arrowRight.svg";
 
-import logoStudio from "../ArtWork/logoStudio.svg";
-import studioBackground from "../ArtWork/studioBackground.jpg";
+import logoStudio from "../ArtWork/LancherixStudioLogoWhiteWhite.png";
+import studioBackground from "../ArtWork/backgroundImage.jpeg";
 
 const slides = [
   { id: 1, color: "#0071e3" },
